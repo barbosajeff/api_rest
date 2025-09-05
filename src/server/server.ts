8f455b747a58e362e392cf469edabb51a0f90356
Jefferson.barbosa <jefferson.barbosa@consensotec.com.br>
2025-09-05 15:14:59 -0300

    adição do Eslint

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,11 +3,12 @@ import express from 'express' ;
 
 const server =  express();
 
+
+
 server.get('/', (req,res)=> {
-    return res.send('Olá, Dev!')
+    return res.send('Olá, Dev!');
 });
 
 
-
-
+// teste 
 export {server};
