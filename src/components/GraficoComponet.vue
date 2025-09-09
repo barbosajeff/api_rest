a4749cc4722c74c1820bab7013ae80d94e37deb5
Jefferson.barbosa <jefferson.barbosa@consensotec.com.br>
2025-09-09 16:24:30 -0300

    projeto_grafico_vue

---
+++ src/components/GraficoComponet.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <apexchart 
+      type="line"
+      height="400"
+      :options="chartOptions"
+      :series="series"
+    />
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "GraficoComponet",
+  components: {  },
+
+  props: {
+    categories: { type: Array, default: () => [] },
+    series: { type: Array, default: () => [] }
+  },
+
+  computed: {
+    chartOptions() {
+      return {
+        chart: {
+          id: "dados-diarios-arrecadacao",
+          toolbar: { show: true }
+        },
+        xaxis: {
+          categories: this.categories
+        },
+        yaxis: {
+          labels: {
+            formatter: (v) => v.toLocaleString("pt-BR")
+          }
+        },
+        tooltip: {
+          y: {
+            formatter: (v) => v.toLocaleString("pt-BR")
+          }
+        }
+      };
+    }
+  }
+};
+</script>
